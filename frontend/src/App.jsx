e1f9c15bb8420f877a7bd7871e0e189b9341dd28
Muhammad Abubakar <abubakarashraf17@gmail.com>
2024-10-28 14:21:09 +0500

    url changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const socketio = io('http://localhost:8080', {
+      const socketio = io('https://instagramclone2.onrender.com', {
         query: {
           userId: user?._id
         },
